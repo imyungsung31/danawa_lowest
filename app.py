bd39d00f023e52ba149ac435b8230c60e6460b4a
imyungsung31 <chainofyoung@gmail.com>
2025-01-02 08:32:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,43 +28,46 @@ if st.button("검색"):
 file_path = f'danawa_lowest_prices_{st.session_state.pcode}.xlsx'
 
 # 엑셀 파일 읽기
-if os.path.exists(file_path):
-    df = pd.read_excel(file_path)
-    # 데이터 필터링
-    st.write("### Filter Data")
-    date_filter = st.date_input("Select date range", [])
-    if date_filter:
-        start_date, end_date = date_filter
-        df = df[(df['날짜 및 시간'] >= pd.to_datetime(start_date)) & (df['날짜 및 시간'] <= pd.to_datetime(end_date))]
+if st.session_state.pcode:
+    if os.path.exists(file_path):
+        df = pd.read_excel(file_path)
+        # 데이터 필터링
+        st.write("### Filter Data")
+        date_filter = st.date_input("Select date range", [])
+        if date_filter:
+            start_date, end_date = date_filter
+            df = df[(df['날짜 및 시간'] >= pd.to_datetime(start_date)) & (df['날짜 및 시간'] <= pd.to_datetime(end_date))]
+            st.dataframe(df)
+
+        # 데이터프레임 표시
+        st.write("### Data")
         st.dataframe(df)
 
-    # 데이터프레임 표시
-    st.write("### Data")
-    st.dataframe(df)
-
-    # 데이터 요약 통계
-    st.write("### Summary Statistics")
-    st.write(df.describe())
+        # 데이터 요약 통계
+        st.write("### Summary Statistics")
+        st.write(df.describe())
 
 
-    # 차트 표시
-    st.write("### Line Chart")
-    st.line_chart(df.set_index('날짜 및 시간'))
+        # 차트 표시
+        st.write("### Line Chart")
+        st.line_chart(df.set_index('날짜 및 시간'))
 
-    st.write("### Bar Chart")
-    st.bar_chart(df.set_index('날짜 및 시간'))
+        st.write("### Bar Chart")
+        st.bar_chart(df.set_index('날짜 및 시간'))
 
-    st.write("### Area Chart")
-    st.area_chart(df.set_index('날짜 및 시간'))
+        st.write("### Area Chart")
+        st.area_chart(df.set_index('날짜 및 시간'))
 
-    # 데이터 다운로드
-    st.write("### Download Data")
-    csv = df.to_csv(index=False).encode('utf-8')
-    st.download_button(
-        label="Download data as CSV",
-        data=csv,
-        file_name='danawa_lowest_prices.csv',
-        mime='text/csv',
-    )
+        # 데이터 다운로드
+        st.write("### Download Data")
+        csv = df.to_csv(index=False).encode('utf-8')
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name='danawa_lowest_prices.csv',
+            mime='text/csv',
+        )
+    else:
+        st.write("No data")
 else:
-    st.write("pcode를 입력해주새요.")
+    st.write("pcode를 입력해주세요.")
